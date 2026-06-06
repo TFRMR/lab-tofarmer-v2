4c198fa286e1750b32263eeeece5d5951450a347
ToFarmer Mastermind <yoanlogic@gmail.com>
2026-06-07 04:40:20 +0700

    kolom-komentar-profi37

--- tofarmer-web/profile.js
+++ tofarmer-web/profile.js
@@ -1457,7 +1457,7 @@ function inisialisasiKomponenNotif() {
     @media (max-width: 768px) {
       #tof-notif-wrapper {
         top: 85px !important;    
-        right: 35% !important;   
+        right: 70% !important;   
         transform: translateX(50%) !important; 
       }
       #box-notif-tof {
