0311c9b96cac4465a11009880195fc6f5ef38133
ToFarmer Mastermind <yoanlogic@gmail.com>
2026-06-09 23:48:07 +0700

    mbah-eko24

--- tofarmer-web/js/supabase-client.js
+++ tofarmer-web/js/supabase-client.js
@@ -3,8 +3,4 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://afpzddwuikvwsedzgsee.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcHpkZHd1aWt2d3NlZHpnc2VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNTExNjEsImV4cCI6MjA5NDcyNzE2MX0.O_IezV8HpBjuZndnXosifts-jS7NnV4VtidyIX1KMHY';
 
-window.supabaseClient = supabaseClient;
-window.db = supabaseClient; // Tambahkan alias untuk cadangan
-console.log("✅ SupabaseClient berhasil dipasang ke window!");
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
