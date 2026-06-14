169dcc44df1c77af498ad50809caa48d83593483
ToFarmer Mastermind <yoanlogic@gmail.com>
2026-06-15 02:19:26 +0700

    ai-mbah-eko16

--- tofarmer-web/mbah_eko_injector.js
+++ tofarmer-web/mbah_eko_injector.js
@@ -310,5 +310,5 @@ observer.observe(document.body, { childList: true, subtree: true });
 
 setTimeout(periksaSkenarioMading, 2000);
 window.addEventListener('load', () => setTimeout(periksaSkenarioMading, 2000));
-});
+);
 }
